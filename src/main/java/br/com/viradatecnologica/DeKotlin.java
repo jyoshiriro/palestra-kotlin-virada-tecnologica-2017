0b1ff5bd43e46123aab5ebe98539ab807bb88267
jyoshiriro <jyoshiriro@gmail.com>
2017-06-29 21:52:31 -0300

    Interoperabilidade Kotlin > Java

---
+++ src/main/java/br/com/viradatecnologica/DeKotlin.java
@@ -0,0 +1,9 @@
+package br.com.viradatecnologica;
+
+public class DeKotlin {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa(1, "José");
+        System.out.println(pessoa.getId());
+        System.out.println(pessoa.getNome());
+    }
+}
